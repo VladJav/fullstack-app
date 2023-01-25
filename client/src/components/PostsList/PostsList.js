5ef884f6cdbf3c26704c9f3adbb5290b0a29ebe7
Vlad <hrom012k@gmail.com>
2023-01-25 23:18:20 +0200

    add posts list

---
+++ client/src/components/PostsList/PostsList.js
@@ -0,0 +1,56 @@
+import InfiniteScroll from 'react-infinite-scroll-component';
+import Box from '@mui/material/Box';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Post, SkeletonPost } from '../Post';
+
+export default function PostsList() {
+    const [posts, setPosts] = useState([]);
+    const [page, setPage] = useState(2);
+    const [hasMore, setHasMore] = useState(true);
+    useEffect(() => {
+        axios.get('http://localhost:8000/api/v1/posts')
+            .then((res) => res.data.posts)
+            .then((post) => { setPosts(posts.concat(post)); });
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+    const fetchData = async () => {
+        const res = await axios.get(`http://localhost:8000/api/v1/posts?page=${page}`);
+        const post = res.data.posts;
+        if (post.length === 0) {
+            setHasMore(false);
+            return;
+        }
+        setPosts(posts.concat(post));
+        setPage(page + 1);
+    };
+    return (
+        <Box>
+            <InfiniteScroll
+              next={fetchData}
+              hasMore={hasMore}
+              loader={<SkeletonPost />}
+              dataLength={posts.length}
+            >
+                {posts.map((e) => {
+                    const {
+                        title,
+                        content,
+                        user,
+                        _id: postId,
+                    } = e;
+                    const { _id: id } = user;
+                    return (
+                        <Post
+                          key={postId}
+                          userId={id}
+                          title={title}
+                          content={content}
+                          author={user.email}
+                        />
+                    );
+                })}
+            </InfiniteScroll>
+        </Box>
+    );
+}
--- client/src/pages/HomePage/HomePage.js
+++ client/src/pages/HomePage/HomePage.js
@@ -2,20 +2,10 @@ import {
     Container,
 } from '@mui/material';
 import Box from '@mui/material/Box';
-import { useEffect, useState } from 'react';
-import axios from 'axios';
-
 import { Header } from '../../components/Header';
-import { Post, SkeletonPost } from '../../components/Post';
+import { PostsList } from '../../components/PostsList';
 
 export default function HomePage() {
-    const [posts, setPosts] = useState();
-    useEffect(() => {
-        axios.get('http://localhost:8000/api/v1/posts')
-            .then((res) => res.data.posts)
-            .then((post) => { setPosts(post); });
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
     return (
         <>
         <Header />
@@ -24,25 +14,7 @@ export default function HomePage() {
         }}
         >
             <Container maxWidth="sm">
-                <Box sx={{
-                    display: 'flex',
-                    flexWrap: 'wrap',
-                }}
-                >
-                    {posts ? (posts.map((e) => {
-                        const { title, content, user } = e;
-                        const { _id: id } = user;
-                        return (
-                            <Post
-                              userId={id}
-                              title={title}
-                              content={content}
-                              author={user.email}
-                            />
-                        );
-                    })
-                    ) : Array.from(new Array(2)).map(() => <SkeletonPost />)}
-                </Box>
+                <PostsList />
             </Container>
         </Box>
         </>
